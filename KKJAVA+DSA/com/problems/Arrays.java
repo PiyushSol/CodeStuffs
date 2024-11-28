747a517064b0405916fc59e065fda9c40fa74d74
Piyush <solankipiyush942@gmail.com>
2024-11-29 00:03:40 +0530

    Code Pushed

--- KKJAVA+DSA/com/problems/Arrays.java
+++ KKJAVA+DSA/com/problems/Arrays.java
@@ -1,2 +1,18 @@
-package com.problems;public class Arrays {
+package com.problems;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int [] res1 = buildArray(new int[]{0, 2, 1, 5, 3, 4});
+        int [] res2 = buildArray(new int[]{5,0,1,2,3,4});
+        System.out.println(java.util.Arrays.toString(res1));
+        System.out.println(java.util.Arrays.toString(res2));
+    }
+
+    public static int[] buildArray(int[] nums){
+        int[] ans = new int[nums.length];
+        for(int i=0;i<ans.length;i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
 }
